4b16ae539550f5ac166e913817d756a35b42ffb1
Tommy Yu <tombahyu@gmail.com>
2021-10-01 20:54:55 -0400

    add login and sign up pages

---
+++ src/pages/LandingPage.js
@@ -0,0 +1,18 @@
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import Link from '@mui/material/Link';
+
+const LandingPage = () => {
+    return (
+        <Box>
+            <Container>
+                <Typography>
+                    How does a train eat? it choo choos!
+                </Typography>
+            </Container>
+        </Box>
+    )
+}
+
+export default LandingPage
