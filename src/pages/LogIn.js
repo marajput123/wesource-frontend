6769bab5de8d206ea53a1d5968b1f51f5ef181c1
Tommy Yu <60073154+TinyEggRoll@users.noreply.github.com>
2021-10-02 00:30:04 -0400

    Add login and sign up pages (#1)
    Deleted Boilerplate code and added pages. Created basic login page.

---
+++ src/pages/LogIn.js
@@ -0,0 +1,81 @@
+import * as React from 'react';
+import { Checkbox, FormControlLabel, Avatar, Button, TextField, Link, Grid, Box, Container, Typography } from '@mui/material'
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import { Link as RouterLink } from 'react-router-dom'
+import Footer from '../components/Footer';
+
+const LogIn = () => {
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        console.log("yee haw1")
+    };
+
+    return (
+        <>
+            <Container component="main" maxWidth="xs">
+                <Box
+                    sx={{
+                        marginTop: 4,
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center'
+                    }}
+                >
+                    <Avatar sx={{ m: 1, bgcolor: 'primary.main' }}>
+                        <LockOutlinedIcon />
+                    </Avatar>
+                    <Typography component="h1" variant="h5">
+                        Log In
+                    </Typography>
+                    <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+                        <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            id="email"
+                            label="Email Address"
+                            name="email"
+                            autoFocus
+                        />
+                        <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            name="password"
+                            label="Password"
+                            type="password"
+                        />
+                        <FormControlLabel
+                            control={<Checkbox value="remember" color="primary" />}
+                            label="Remember me"
+                        />
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            sx={{ mt: 3, mb: 2 }}
+                        >
+                            Log in
+                        </Button>
+                        <Grid container mt={4}>
+                            <Grid item xs>
+                                <Link variant="body2">
+                                    Forgot password?
+                                </Link>
+                            </Grid>
+                            <Grid item>
+                                <Link variant='body2' component={RouterLink} to='/signup'>
+                                    {"Don't have an account? Sign Up"}
+                                </Link>
+                            </Grid>
+                        </Grid>
+                    </Box>
+                </Box>
+            </Container>
+            <Footer />
+        </>
+    );
+}
+
+export default LogIn
