bc500b71bec0651343027565cc07f40319e1299c
Tommy Yu <60073154+TinyEggRoll@users.noreply.github.com>
2021-11-17 21:38:53 -0500

    product dashboard page missing bgcolor (#18)
    * product dashboard page missing bgcolor
    
    * minor changes to routing on app.js and product dashboard.js
    
    * resolved mainproductbox redirect, and routing on app for productdashboard
    
    Co-authored-by: Austin Smith <77940344+AustinMorganSmith@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,21 +1,43 @@
 import * as React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import {Container, Paper} from '@mui/material'
 import LandingPage from './pages/LandingPage';
 import LogIn from './pages/login';
 import Profile from './pages/profile';
 import NavBar from './components/navbar/NavBar';
 import Footer from './components/Footer';
 import { Box } from '@mui/system';
-import SearchProducts from './pages/SearchProducts'
-import theme from './theme';
-import { ThemeProvider } from '@mui/material/styles';
-
-
 
 function App() {
   return (
     <>
+      <Router>
+        <NavBar />
+        <Box
+          sx={{
+            height: '100vh',
+            display: 'flex',
+            minHeight: '700px',
+            flexDirection: 'column',
+          }}
+        >
+          <Container maxWidth="lg" sx={{ flexGrow: 1 }}>
+            <Switch>
+              {/* Landing Page */}
+              <Route exact path="/" component={LandingPage} />
+              {/* Login Page */}
+              <Route exact path="/login" component={LogIn} />
+              {/* Search Products Page */}
+              <Route exact path="/search-products" component={SearchProducts} />
+              {/* Product Dashboard */}
+              <Route
+                exact
+                path="/dashboard/:productDashboardID"
+                component={ProductDashboard}
+              />
+            </Switch>
+          </Container>
+          <Footer />
+        </Box>
+      </Router>
       <ThemeProvider theme={theme}>
         <Paper sx={{bgcolor:"background.default"}}>
           <Router>
@@ -39,10 +61,7 @@ function App() {
         </Paper>
       </ThemeProvider>
     </>
-
   );
 }
-
-
 
 export default App;
